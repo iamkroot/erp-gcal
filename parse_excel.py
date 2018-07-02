53fe93b00cc42583fff61a731464b029078a7bb8
mach64 <userisro@gmail.com>
2018-07-02 20:32:30 +0530

    parse_excel: Fix incorrect section instructor names
    Rows with blank section number cells in timetable caused the previous
    section with same number to be overwritten. Added new sec_num_counter
    to fix this. Also added global variables for easy config of column
    indices.

--- parse_excel.py
+++ parse_excel.py
@@ -4,6 +4,7 @@ import re
 
 
 def title_except(s, exceptions=['I', 'II', 'III', 'IV']):
+    """Convert string to Title Case"""
     word_list = re.split(' ', s)
     final = [word_list[0].capitalize()]
     for word in word_list[1:]:
@@ -11,41 +12,74 @@ def title_except(s, exceptions=['I', 'II', 'III', 'IV']):
     return " ".join(final)
 
 
-file = 'tt.xlsx'
+TOP_LEFT_CELL = 'A3'
+LAST_COL = 'M'
+ROWS = {  # 0 indexed column indices
+    'C_NUM': 1,
+    'C_TITLE': 2,
+    'SEC_NUM': 6,
+    'INSTR_NAME': 7,
+    'ROOM': 8,
+    'DAYS': 9,
+    'HOURS': 10,
+    'COMPRE': 12
+}
 
-sheet = load_workbook(filename=file).active
-req_area = sheet['A2':'H1055']
-course_db = {}
-for row in req_area:
-    if not row[3].value:
-        continue
-    if row[0].value:
-        course = {
-            'name': title_except(row[1].value),
-            'sections': {},
-            'compre': {
-                'date': row[7].value.split()[0] if row[7].value else None,
-                'session': row[7].value.split()[1] if row[7].value else None
+
+def parse(sheet):
+    req_area = sheet[TOP_LEFT_CELL: LAST_COL + str(sheet.max_row)]
+    course_db = {}
+
+    for row in req_area:
+        if not row[ROWS['INSTR_NAME']].value:
+            continue  # blank row
+
+        # new Course
+        if row[ROWS['C_NUM']].value:
+            compre = row[ROWS['COMPRE']].value
+            course = {
+                'name': title_except(row[ROWS['C_TITLE']].value),
+                'sections': {},
+                'compre': {
+                    'date': compre.split()[0] if compre else None,
+                    'session': compre.split()[1] if compre else None
+                }
             }
-        }
-        course_db[row[0].value] = course
-        sec_type = 'L'
+            course_db[row[ROWS['C_NUM']].value] = course  # add to course
+            sec_type = 'L'
+            sec_num_counter = 1
 
-    if not row[0].value and row[1].value:
-        sec_type = row[1].value[0]
+        # new Tutorial or Practical section
+        if not row[ROWS['C_NUM']].value and row[ROWS['C_TITLE']].value:
+            sec_type = row[ROWS['C_TITLE']].value[0]
+            sec_num_counter = 1
 
-    if row[4].value or row[1].value:
-        sec_num = row[2].value or 1
-        section = {
-            'instructors': []
-        }
-        course['sections'][sec_type + str(sec_num)] = section
+        # new Section
+        if (row[ROWS['INSTR_NAME']].value and row[ROWS['ROOM']].value) or \
+                row[ROWS['C_TITLE']].value:
+            sec_num = row[ROWS['SEC_NUM']].value or sec_num_counter
+            section = {
+                'instructors': []
+            }
+            course['sections'][sec_type + str(sec_num)] = section
+            sec_num_counter += 1
 
-    section['instructors'].append(row[3].value)
+        for ind, key in enumerate(['room', 'days', 'hours'], ROWS['ROOM']):
+            if key not in section:
+                section[key] = str(row[ind].value) if row[ind].value else None
 
-    for ind, key in enumerate(['room', 'days', 'hours'], 4):
-        if key not in section:
-            section[key] = str(row[ind].value) if row[ind].value else None
+        section['instructors'].append(row[ROWS['INSTR_NAME']].value)
+    return course_db
 
-with open('timetable.json', 'w') as f:
-    f.write(json.dumps(course_db, indent=4))
+
+def main():
+    file = 'TT/Draft Timetable 2018 - 19 I Sem.xlsx'
+    sheet = load_workbook(filename=file).active
+    course_db = parse(sheet)
+
+    with open(f'{file}.json', 'w') as f:
+        f.write(json.dumps(course_db, indent=4))
+
+
+if __name__ == '__main__':
+    main()
